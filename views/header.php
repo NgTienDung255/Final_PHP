3e514f5e84df8577af9d0292b5cf60c018073cdd
TUANNN <tuannnm510@gmail.com>
2024-12-20 23:44:06 +0700

    tên nhà hàng

--- views/header.php
+++ views/header.php
@@ -68,7 +68,7 @@ if (isset($_GET["page_layout"])) {
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
                 <a href="" class="navbar-brand p-0">
                     <h1 class="text-primary m-0">
-                        <i class="fa fa-utensils me-3"></i>Restoran
+                        <i class="fa fa-utensils me-3"></i>BA'S BEST HPOT
                     </h1>
                     <!-- <img src="../views/img/logo.png" alt="Logo"> -->
                 </a>
